a2a6aadd564af8c00d5f72d95e802502dae1d66a
satel1ite <chernyavskiy.as@gmail.com>
2025-12-12 00:14:03 +0300

    Update task2.cpp

--- hw2/task2.cpp
+++ hw2/task2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
-using namespace std;
+
 
 struct Rectangle {
     int lx, ly, rx, ry;
@@ -9,14 +9,14 @@ struct Rectangle {
 
 int main() {
     long long N;
-    cin >> N;
+    std::cin >> N;
     if (N <= 0) {
-        cout << "0 0 0 0\n";
+        std::cout << "0 0 0 0\n";
         return 0;
     }
 
     Rectangle r;
-    cin >> r.lx >> r.ly >> r.rx >> r.ry;
+    std::cin >> r.lx >> r.ly >> r.rx >> r.ry;
 
     int lx_min = r.lx;
     int ly_max = r.ly;
@@ -24,17 +24,17 @@ int main() {
     int ry_min = r.ry;
 
     for (int i = 1; i < N; i++) {
-        cin >> r.lx >> r.ly >> r.rx >> r.ry;
-        lx_min = min(lx_min, r.lx);
-        ly_max = max(ly_max, r.ly);
-        rx_max = max(rx_max, r.rx);
-        ry_min = min(ry_min, r.ry);  
+        std::cin >> r.lx >> r.ly >> r.rx >> r.ry;
+        lx_min = std::min(lx_min, r.lx);
+        ly_max = std::max(ly_max, r.ly);
+        rx_max = std::max(rx_max, r.rx);
+        ry_min = std::min(ry_min, r.ry);  
     }
 
     if (lx_min == rx_max || ly_max == ry_min) {
-        cout << lx_min << " " << ly_max << " 0 0\n";
+        std::cout << lx_min << " " << ly_max << " 0 0\n";
     } else {
-        cout << lx_min << " " << ly_max << " " << rx_max << " " << ry_min << "\n";
+        std::cout << lx_min << " " << ly_max << " " << rx_max << " " << ry_min << "\n";
     }
 
     return 0;
