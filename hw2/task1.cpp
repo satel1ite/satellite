e1bc5d8662de51ec0d27f2ea8aa24a75d3cd931b
satel1ite <chernyavskiy.as@gmail.com>
2025-12-11 23:53:45 +0300

    Update task1.cpp

--- hw2/task1.cpp
+++ hw2/task1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
-using namespace std;
 
 struct Rectangle {
     int lx, ly, rx, ry;
@@ -9,14 +8,14 @@ struct Rectangle {
 
 int main() {
     long long N;
-    cin >> N;
+    std::cin >> N;
     if (N <= 0) {
-        cout << "0 0 0 0\n";
+        std::cout << "0 0 0 0\n";
         return 0;
     }
 
     Rectangle r;
-    cin >> r.lx >> r.ly >> r.rx >> r.ry;
+    std::cin >> r.lx >> r.ly >> r.rx >> r.ry;
 
     int lx1 = r.lx;
     int ly1 = r.ly;
@@ -24,21 +23,21 @@ int main() {
     int ry1 = r.ry;
 
     for (int i = 1; i < N; i++) {
-        cin >> r.lx >> r.ly >> r.rx >> r.ry;
-        lx1 = max(lx1, r.lx);
-        ly1 = min(ly1, r.ly);
-        rx1 = min(rx1, r.rx);
-        ry1 = max(ry1, r.ry);
+        std::cin >> r.lx >> r.ly >> r.rx >> r.ry;
+        lx1 = std::max(lx1, r.lx);
+        ly1 = std::min(ly1, r.ly);
+        rx1 = std::min(rx1, r.rx);
+        ry1 = std::max(ry1, r.ry);
     }
 
     if (lx1 <= rx1 && ly1 >= ry1) {
         if (lx1 == rx1 || ly1 == ry1) {
-            cout << lx1 << " " << ly1 << " 0 0\n";
+            std::cout << lx1 << " " << ly1 << " 0 0\n";
         } else {
-            cout << lx1 << " " << ly1 << " " << rx1 << " " << ry1 << "\n";
+            std::cout << lx1 << " " << ly1 << " " << rx1 << " " << ry1 << "\n";
         }
     } else {
-        cout << "0 0 0 0\n";
+        std::cout << "0 0 0 0\n";
     }
 
     return 0;
